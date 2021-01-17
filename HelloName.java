5694c4988c3e1d5ac831b7837375463df3e88460
PavelHerasiuk <73124757+PavelHerasiuk@users.noreply.github.com>
2021-01-17 16:12:31 +0300

    Update HelloName.java

--- HelloName.java
+++ HelloName.java
@@ -3,7 +3,7 @@ package by.epam.task1.main;
 public class HelloName {
 
     public static String sayHello(String s) {
-        return "Hello, " + s;
+         return "Hello, " + s + "\nNice to met you";
     }
 
     public static void main(String[] args) {
